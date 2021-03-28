3ad6eee999bfffce8ec25e5ff2f36d6017b5c7fa
Oluwatobiju Judah <tobijudah@gmail.com>
2021-03-28 10:55:35 +0100

    feat: Nav component

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import S from "./Nav.module.scss";
+
+const Nav: React.FC = () => {
+	return (
+		<nav className={S.nav}>
+			<div className={S.upper}>
+				<p>Me</p>
+				<p>Nu</p>
+			</div>
+			<div className={S.lower}>
+				<p className={S.lowerText}>
+					Barn <span className={S.and}>&</span> Bed
+				</p>
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
