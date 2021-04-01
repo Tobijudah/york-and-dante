f7862652894ac8630967baed2cf8607110257a60
Oluwatobiju Judah <tobijudah@gmail.com>
2021-04-01 23:01:21 +0100

    feat: add background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import "./App.css";
+import S from "./App.module.scss";
 import Nav from "./components/Nav/Nav";
+import video from './videos/home.mp4'
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Nav />
-		</div>
+			<div className={S.app} data-scroll-container>
+				<section className={S.home}>
+					<div className={S.videoWrapper}>
+						<video
+							loop
+							muted
+							autoPlay
+							src={video}
+							className={S.video}
+						></video>
+					</div>
+				</section>
+			</div>
+		</>
 	);
 }
 
