6625a01006b43d0718341aaaa7fa9fd29f467e3f
Oluwatobiju Judah <tobijudah@gmail.com>
2021-04-02 11:16:09 +0100

    feat: add SectionOne component

---
+++ src/components/SectionOne/SectionOne.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../Button/Button";
+import S from "./SectionOne.module.scss";
+import video from "../../videos/home.mp4";
+
+const SectionOne: React.FC = () => {
+	return (
+		<section className={S.section}>
+			<div className={S.textWrapper}>
+				<h1 className={S.text}>
+					Chairs that <br /> defy the norm.
+				</h1>
+				<div className={S.button}>
+					<Button color="greige" />
+				</div>
+			</div>
+			<div className={S.videoWrapper}>
+				<video
+					loop
+					muted
+					autoPlay
+					src={video}
+					className={S.video}
+				></video>
+			</div>
+		</section>
+	);
+};
+
+export default SectionOne;
