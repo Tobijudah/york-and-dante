a2b7a5283ef7a8fc2a0a152c33b1ac448b3eb1d9
Oluwatobiju Judah <tobijudah@gmail.com>
2021-04-02 11:11:46 +0100

    feat: add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import S from "./Button.module.scss";
+import { ReactComponent as Arrow } from "../../svgs/arrow.svg";
+
+type ButtonProps = {
+	text?: boolean;
+	color: "red" | "greige";
+};
+
+const Button: React.FC<ButtonProps> = ({ text, color }) => {
+	return (
+		<button className={`${S[color]} ${S[text ? "large" : ""]}`}>
+			{text ? (
+				<p className={S.text}>Buy</p>
+			) : (
+				<Arrow stroke={color === "greige" ? "#000000" : "#ffffff"} />
+			)}
+		</button>
+	);
+};
+
+export default Button;
