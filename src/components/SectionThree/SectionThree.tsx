ac542a4f59df9c2c78a8c9fd692868081ca53d53
Oluwatobiju Judah <tobijudah@gmail.com>
2021-04-03 22:20:09 +0100

    feat: add SectionThree component

---
+++ src/components/SectionThree/SectionThree.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import S from "./SectionThree.module.scss";
+import image from "../../images/section-3.png";
+import { ReactComponent as RotatingText } from "../../svgs/rotating-text.svg";
+
+const SectionThree: React.FC = () => {
+	return (
+		<section data-scroll-section>
+			<div className={S.section}>
+				<div data-scroll data-speed-2 className={S.textWrapper}>
+					<h2 className={S.title}>
+						A minmalistic approach to design
+					</h2>
+					<p className={S.text}>
+						Mutley, you snickering, floppy eared hound. When courage
+						is needed, you’re never around. Those medals you wear on
+						your moth-eaten chest should be there for bungling at
+						which you are best.
+					</p>
+				</div>
+				<div data-scroll data-speed-3 className={S.imageWrapper}>
+					<img src={image} className={S.image} alt="" />
+					<RotatingText className={S.svg} />
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default SectionThree;
