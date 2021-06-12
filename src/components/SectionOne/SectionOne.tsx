10f390fa8d5061f473ad6a25598b74665254ced9
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-12 14:57:02 +0100

    feat: update section one texts

--- src/components/SectionOne/SectionOne.tsx
+++ src/components/SectionOne/SectionOne.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Button from "../Button/Button";
 import S from "./SectionOne.module.scss";
-import video from "../../videos/section-one.mp4";
 
 const SectionOne: React.FC = () => {
 	return (
 		<section data-scroll-section className={S.section}>
 			<div className={S.textWrapper}>
 				<h1 className={S.text}>
-					Chairs that <br /> defy the norm.
+					The Credenza <sup className={S.sup}>&trade;</sup>
 				</h1>
+				<p className={S.subText}>
+					Another piece from award winning furniture maker Barn & Bed
+				</p>
 				<div className={S.button}>
 					<Button color="greige" />
 				</div>
@@ -21,7 +23,7 @@ const SectionOne: React.FC = () => {
 					autoPlay
 					playsInline
 					className={S.video}
-					src='https://res.cloudinary.com/tobijudah/video/upload/v1622885626/barn-and-bed/videos/section-one_vnmqej.mp4'
+					src="https://res.cloudinary.com/tobijudah/video/upload/v1622885626/barn-and-bed/videos/section-one_vnmqej.mp4"
 				></video>
 			</div>
 		</section>
