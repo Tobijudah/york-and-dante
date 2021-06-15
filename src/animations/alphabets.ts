60394d8917f5aa90286b8635a5ab3f7fab058087
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-15 20:22:43 +0100

    feat: update alphabets animation

--- src/animations/alphabets.ts
+++ src/animations/alphabets.ts
@@ -4,19 +4,30 @@ gsap.config({
 });
 
 const AlphabetsAnimation = (
+	delay: number,
 	element: gsap.TweenTarget,
 	letter: string,
 	index: string
 ) => {
 	const indexNumber = Number(index);
 	const letterNumber = Number(letter);
-	const unit = window.innerWidth > 600 ? 'vh' : 'rem';
+	const unit = window.innerWidth > 600 ? "vh" : "rem";
 	const translateNumber = window.innerWidth > 600 ? -7.2267 : -1.626;
-	gsap.to(element, {
-		delay: 0.2 * indexNumber,
-		duration: letterNumber < 6 ? 0.3 * letterNumber : 0.1 * letterNumber,
-		transform: `translateY(${translateNumber * letterNumber}${unit})`,
-	});
+
+	const alphabets = gsap.timeline({ delay: delay + 1 });
+
+	alphabets
+		.to(element, {
+			delay: 0.2 * indexNumber,
+			duration:
+				letterNumber < 6 ? 0.3 * letterNumber : 0.1 * letterNumber,
+			transform: `translateY(${translateNumber * letterNumber}${unit})`,
+		})
+		.eventCallback("onComplete", () => {
+			alphabets.kill();
+		});
+
+	return alphabets.totalDuration()
 };
 
 export default AlphabetsAnimation;
