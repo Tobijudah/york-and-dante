c7969e57872032dcfa7c21ff8ae494b72fabcdc7
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-15 20:25:53 +0100

    feat: add updates & animations to Preloader component

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,86 +1,114 @@
 import S from "./Preloader.module.scss";
-import React, { useEffect } from "react";
 import Alphabets from "../Alphabets/Alphabets";
+import React, { useEffect, useRef } from "react";
 import useRefArray from "../../hooks/useRefArray";
+import FadeOutAnimation from "../../animations/fade-out";
 import AlphabetsAnimation from "../../animations/alphabets";
+import PreloaderAnimation from "../../animations/preloader";
 
-const Preloader = () => {
+type PreloaderProps = {
+	setPreloaded: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Preloader = ({ setPreloaded }: PreloaderProps) => {
+	const wordRef = useRef(null);
 	const letterRefs = useRefArray(7);
+	const preloaderRef = useRef(null);
+	const preloaderContainerRef = useRef(null);
 
 	useEffect(() => {
 		const preload = () => {
+			const preloaderDuration = PreloaderAnimation([
+				preloaderRef.current,
+				wordRef.current,
+			]);
+			let alphabetsDuration: number | undefined;
 			letterRefs &&
 				letterRefs.forEach((ref) => {
-					AlphabetsAnimation(
+					alphabetsDuration = AlphabetsAnimation(
+						preloaderDuration,
 						ref.current,
 						ref.current.getAttribute("data-letter")!,
 						ref.current.getAttribute("data-index")!
 					);
 				});
+			if (!!alphabetsDuration) {
+				FadeOutAnimation(
+					alphabetsDuration + preloaderDuration,
+					preloaderContainerRef.current,
+					() => setPreloaded(true)
+				);
+			}
 		};
-		window.addEventListener('resize', preload);
+		window.addEventListener("resize", preload);
 		setTimeout(() => {
 			preload();
 		}, 1500);
+
+		return () => window.removeEventListener("resize", preload);
 	}, [letterRefs]);
 
 	return (
-		<div className={S.preloader}>
-			<div className={S.barn}>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={1}
-						letterIndex={2}
-						ref={letterRefs[0]}
-					/>
-				</div>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={2}
-						letterIndex={1}
-						ref={letterRefs[1]}
-					/>
-				</div>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={3}
-						letterIndex={18}
-						ref={letterRefs[2]}
-					/>
-				</div>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={4}
-						letterIndex={14}
-						ref={letterRefs[3]}
-					/>
+		<section ref={preloaderContainerRef} className={S.preloaderContainer}>
+			<div ref={preloaderRef} className={S.preloader}>
+				<div ref={wordRef} className={S.word}>
+					<div className={S.barn}>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={1}
+								letterIndex={2}
+								ref={letterRefs[0]}
+							/>
+						</div>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={2}
+								letterIndex={1}
+								ref={letterRefs[1]}
+							/>
+						</div>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={3}
+								letterIndex={18}
+								ref={letterRefs[2]}
+							/>
+						</div>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={4}
+								letterIndex={14}
+								ref={letterRefs[3]}
+							/>
+						</div>
+					</div>
+					<p className={S.and}>&</p>
+					<div className={S.bed}>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={5}
+								letterIndex={2}
+								ref={letterRefs[4]}
+							/>
+						</div>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={6}
+								letterIndex={5}
+								ref={letterRefs[5]}
+							/>
+						</div>
+						<div className={S.letter}>
+							<Alphabets
+								dataIndex={7}
+								letterIndex={4}
+								ref={letterRefs[6]}
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
-			<p className={S.and}>&</p>
-			<div className={S.bed}>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={5}
-						letterIndex={2}
-						ref={letterRefs[4]}
-					/>
-				</div>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={6}
-						letterIndex={5}
-						ref={letterRefs[5]}
-					/>
-				</div>
-				<div className={S.letter}>
-					<Alphabets
-						dataIndex={7}
-						letterIndex={4}
-						ref={letterRefs[6]}
-					/>
-				</div>
-			</div>
-		</div>
+		</section>
 	);
 };
 
