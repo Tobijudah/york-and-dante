1add8c54b5e7f39db5eff0fea135edda7e4dc027
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-16 10:12:27 +0100

    feat: add shell of credits component

---
+++ src/components/Credits/Credits.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  S from './Credits.module.scss'
+
+const Credits = () => {
+  return (
+    <div className={S.credits}>
+      
+    </div>
+  )
+}
+
+export default Credits;
