26abad8e69bf7abe1727113128997c9b420ca436
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-18 15:47:13 +0100

    chore: update Cart component with forwardRef

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import S from "./Cart.module.scss";
 
-const Cart: React.FC = () => {
+const Cart = React.forwardRef<HTMLDivElement>((_, ref) => {
 	return (
-		<div className={S.cart}>
-			<p className={S.text}>Cart</p>
-			<span className={S.number}>0</span>
+		<div className={`${S.cart} split-text hidden-init`}>
+			<div ref={ref} className={S.cartDiv}>
+				<p className={S.text}>Cart</p>
+				<span className={S.number}>0</span>
+			</div>
 		</div>
 	);
-};
+});
 
 export default Cart;
