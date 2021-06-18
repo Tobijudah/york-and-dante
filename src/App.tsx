819429a59ef7511dc9d4922c4911317aa33b2af6
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-18 15:50:55 +0100

    feat: add app updates
    new refs
    splitting
    intro animation w/ timeout
    remove body from preloaded condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import S from "./App.module.scss";
+import Splitting from "splitting";
+import "splitting/dist/splitting.css";
 import Nav from "./components/Nav/Nav";
 import Cart from "./components/Cart/Cart";
 import Menu from "./components/Menu/Menu";
+import IntroAnimation from "./animations/intro";
 import LocomotiveScroll from "locomotive-scroll";
 import React, { useEffect, useRef, useState } from "react";
+import Preloader from "./components/Preloader/Preloader";
 import SectionOne from "./components/SectionOne/SectionOne";
 import SectionTwo from "./components/SectionTwo/SectionTwo";
 import SectionSix from "./components/SectionSix/SectionSix";
@@ -13,9 +17,10 @@ import SectionThree from "./components/SectionThree/SectionThree";
 import SectionSeven from "./components/SectionSeven/SectionSeven";
 import SectionEight from "./components/SectionEight/SectionEight";
 import "../node_modules/locomotive-scroll/src/locomotive-scroll.scss";
-import Preloader from "./components/Preloader/Preloader";
 
 function App() {
+	const navRef = useRef(null);
+	const cartRef = useRef(null);
 	const scrollRef = useRef(null);
 	const [open, setOpen] = useState<boolean>(false);
 	const [preloaded, setPreloaded] = useState<boolean>(false);
@@ -34,33 +39,31 @@ function App() {
 					smooth: true,
 				},
 			});
-		}
+			scroll.stop();
+			setTimeout(() => {
+				scroll.start();
+				IntroAnimation([navRef.current, cartRef.current]);
+			}, 10000);
+			// 10000: time for section-1 animation to start
+		} else Splitting();
 	}, [preloaded]);
 
 	return (
 		<>
 			{!preloaded && <Preloader setPreloaded={setPreloaded} />}
-			{preloaded && (
-				<>
-					<Nav onClick={() => setOpen(!open)} />
-					<Cart />
-					<Menu open={open} onClick={() => setOpen(!open)} />
-					<div
-						ref={scrollRef}
-						className={S.app}
-						data-scroll-container
-					>
-						<SectionOne />
-						<SectionTwo />
-						<SectionThree />
-						<SectionFour />
-						<SectionFive />
-						<SectionSix />
-						<SectionSeven />
-						<SectionEight />
-					</div>
-				</>
-			)}
+			<Cart ref={cartRef} />
+			<Nav ref={navRef} onClick={() => setOpen(!open)} />
+			<Menu open={open} onClick={() => setOpen(!open)} />
+			<div ref={scrollRef} className={S.app} data-scroll-container>
+				<SectionOne preloaded={preloaded} />
+				<SectionTwo />
+				<SectionThree />
+				<SectionFour />
+				<SectionFive />
+				<SectionSix />
+				<SectionSeven />
+				<SectionEight />
+			</div>
 		</>
 	);
 }
