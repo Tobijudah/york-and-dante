d92066f46f2108c8339b64ce599a4271699ffa06
Oluwatobiju Judah <tobijudah@gmail.com>
2021-06-26 21:25:58 +0100

    feat: add section three image reveal animation

--- src/components/SectionThree/SectionThree.tsx
+++ src/components/SectionThree/SectionThree.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import gsap from "gsap";
 import S from "./SectionThree.module.scss";
 import image from "../../images/section-3.png";
+import React, { useEffect, useRef } from "react";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import { ReactComponent as RotatingText } from "../../svgs/rotating-text.svg";
 
 const SectionThree: React.FC = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const imageRef = useRef<HTMLImageElement>(null);
+	const isOnScreen = useIntersectionObserver(ref, 0.3);
+
+	useEffect(() => {
+		if (isOnScreen && imageRef.current) {
+			gsap.to(imageRef.current, {
+				scale: 1,
+				duration: 2,
+				ease: "expo.inOut",
+				clipPath: "inset(0% 0% 0% 0%)",
+			});
+		}
+	}, [isOnScreen, imageRef.current]);
+
 	return (
 		<section data-scroll-section>
 			<div className={S.section}>
-				<div data-scroll data-speed-2 className={S.textWrapper}>
+				<div className={S.textWrapper}>
 					<h2 className={S.title}>
 						A minmalistic approach to chair design
 					</h2>
@@ -20,8 +37,15 @@ const SectionThree: React.FC = () => {
 						for public spaces.
 					</p>
 				</div>
-				<div data-scroll data-speed-3 className={S.imageWrapper}>
-					<img src={image} className={S.image} alt="" />
+				<div ref={ref} className={S.imageWrapper}>
+					<div className={S.imageContainer}>
+						<img
+							alt=""
+							src={image}
+							ref={imageRef}
+							className={S.image}
+						/>
+					</div>
 					<RotatingText
 						width="20.24vh"
 						height="20.24vh"
