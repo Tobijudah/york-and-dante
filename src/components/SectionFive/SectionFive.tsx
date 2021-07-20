e6163abbb61e72cc061a17b04d70549c1ddbe185
Oluwatobiju Judah <tobijudah@gmail.com>
2021-07-20 16:48:51 +0100

    feat: update text parallax + formatting

--- src/components/SectionFive/SectionFive.tsx
+++ src/components/SectionFive/SectionFive.tsx
@@ -5,9 +5,12 @@ import image from "../../images/section-5.png";
 import { ReactComponent as Line } from "../../svgs/line.svg";
 
 const SectionFive: React.FC = () => {
+	const title1ScrollSpeed = window.innerWidth < 1024 ? 2.5 : 1.2;
+	const title2ScrollSpeed = window.innerWidth < 1024 ? -5 : -1.2;
+
 	return (
 		<section data-scroll-section>
-			<div className={S.section}>
+			<div id="target-element" className={S.section}>
 				<Marquee
 					speed={15}
 					pauseOnHover
@@ -19,12 +22,12 @@ const SectionFive: React.FC = () => {
 					<p className={S.marqueeText}>Products</p>
 					<p className={S.marqueeText}>Products</p>
 				</Marquee>
-				<div id="target-element" className={S.main}>
+				<div className={S.main}>
 					<h2
 						data-scroll
 						className={S.title1}
-						data-scroll-speed={1.2}
 						data-scroll-direction="horizontal"
+						data-scroll-speed={title1ScrollSpeed}
 					>
 						The
 					</h2>
@@ -32,9 +35,9 @@ const SectionFive: React.FC = () => {
 					<h2
 						data-scroll
 						className={S.title2}
-						data-scroll-speed={-1.2}
 						data-scroll-direction="horizontal"
 						data-scroll-target="#target-element"
+						data-scroll-speed={title2ScrollSpeed}
 					>
 						Credenza
 					</h2>
@@ -42,12 +45,20 @@ const SectionFive: React.FC = () => {
 				<div className={S.sub}>
 					<p className={S.subText}>Comfy sofas</p>
 					<div className={S.subRow}>
-						<Line width='11.22vh' height='0.33vh' className={S.line1} />
+						<Line
+							width="11.22vh"
+							height="0.33vh"
+							className={S.line1}
+						/>
 						<p className={S.subText}>Cylindrical design</p>
 					</div>
 					<div className={S.subRow}>
 						<p className={S.subText}>Arc back hinge</p>
-						<Line width='11.22vh' height='0.33vh' className={S.line2} />
+						<Line
+							width="11.22vh"
+							height="0.33vh"
+							className={S.line2}
+						/>
 					</div>
 				</div>
 				<div className={S.textWrapper}>
