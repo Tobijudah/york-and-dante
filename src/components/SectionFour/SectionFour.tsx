f43d197e2ec74e4592969b1025bbfc4e53773428
Oluwatobiju Judah <tobijudah@gmail.com>
2021-07-20 13:10:30 +0100

    feat: add scribble svg animation

--- src/components/SectionFour/SectionFour.tsx
+++ src/components/SectionFour/SectionFour.tsx
@@ -1,20 +1,36 @@
-import React from "react";
+import gsap from "gsap";
 import S from "./SectionFour.module.scss";
+import React, { useEffect, useRef } from "react";
 import { ReactComponent as Corner } from "../../svgs/corner.svg";
 import { ReactComponent as Line } from "../../svgs/vertical-line.svg";
 import { ReactComponent as Scribble } from "../../svgs/scribble.svg";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 const SectionFour: React.FC = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const isOnScreen = useIntersectionObserver(ref, 0.25);
+
+	useEffect(() => {
+		if (isOnScreen) {
+			gsap.to('#SVGRef', {
+				delay: 0.5,
+				duration: 3,
+				ease: "expo.out",
+				strokeDashoffset: 0,
+			});
+		}
+	}, [isOnScreen]);
 	return (
 		<section data-scroll-section>
 			<div className={S.section}>
 				<div className={S.left}>
 					<Corner width="5.5vh" height="5.5vh" className={S.corner} />
-					<h2 className={S.title}>
+					<h2 ref={ref} className={S.title}>
 						Closer to <span className={S.bronze}>nature</span> than
 						you imagine
 					</h2>
 					<Scribble
+						id={'SVGRef'}
 						width="43.68vh"
 						height="23.59vh"
 						className={S.scribble}
@@ -25,7 +41,7 @@ const SectionFour: React.FC = () => {
 						Made of Eco-Friendly materials sewn into the hem of
 						every piece
 					</h3>
-					<Line height='5.67vh' className={S.line} />
+					<Line height="5.67vh" className={S.line} />
 					<p className={S.text}>
 						Our minimalist Barn & Bed chairs is crafted from
 						American poplar and metal tubing with a leather sling.
