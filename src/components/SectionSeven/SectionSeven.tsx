1667361da5d5acd46ac7f20aa954bdc05b245431
Oluwatobiju Judah <tobijudah@gmail.com>
2021-07-20 15:18:27 +0100

    feat: update section seven image animation

--- src/components/SectionSeven/SectionSeven.tsx
+++ src/components/SectionSeven/SectionSeven.tsx
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import gsap from "gsap";
+import Button from "../Button/Button";
 import S from "./SectionSeven.module.scss";
 import image from "../../images/section-7.png";
-import Button from "../Button/Button";
+import React, { useEffect, useRef, useState } from "react";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 const SectionSeven = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const imageRef = useRef<HTMLImageElement>(null);
 	const [amount, setAmount] = useState<number>(1);
+	const isOnScreen = useIntersectionObserver(ref, 0.25);
+
+	useEffect(() => {
+		if (isOnScreen && imageRef.current) {
+			gsap.to(imageRef.current, {
+				scale: 1,
+				duration: 4,
+				ease: "expo.out",
+				clipPath: "inset(0% 0% 0% 0%)",
+			});
+		}
+	}, [isOnScreen, imageRef.current]);
 
 	const add = () => setAmount((amount) => amount + 1);
 
@@ -18,7 +34,15 @@ const SectionSeven = () => {
 		<section data-scroll-section>
 			<div className={S.section}>
 				<div className={S.left}>
-					<img className={S.image} src={image} alt="" />
+					<div ref={ref} className={S.imageContainer}>
+						<img
+							alt=""
+							src={image}
+							ref={imageRef}
+							className={S.image}
+						/>
+					</div>
+
 					<div className={S.button}>
 						<Button text color="red" />
 					</div>
