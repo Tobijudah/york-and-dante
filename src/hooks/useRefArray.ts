03d252a14b407c70cd202b677fd79c2f44d32790
Oluwatobiju Judah <tobijudah@gmail.com>
2021-07-24 02:29:19 +0100

    chore: add user defined types for useRefArray hook

--- src/hooks/useRefArray.ts
+++ src/hooks/useRefArray.ts
@@ -1,16 +1,16 @@
 import { createRef, useEffect, useState } from "react";
 
-const useRefArray = (length: number) => {
-	const [refs, setRefs] = useState<React.MutableRefObject<HTMLDivElement>[]>([]);
+function useRefArray<S>(length: number) {
+	const [refs, setRefs] = useState<React.MutableRefObject<S>[]>([]);
 
 	useEffect(() => {
 		setRefs((refs) =>
 			Array(length)
 				.fill(0)
-				.map((_, i) => refs[i] || createRef<HTMLDivElement>())
+				.map((_, i) => refs[i] || createRef<S>())
 		);
 	}, [length]);
 	return refs;
-};
+}
 
 export default useRefArray;
