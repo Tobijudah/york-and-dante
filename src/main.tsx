83530552453730c7621f8b671da957afb9e64111
Oluwatobiju Judah <tobijudah@gmail.com>
2021-12-15 19:36:02 +0100

    chore: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './App'
+import "./index.scss";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
