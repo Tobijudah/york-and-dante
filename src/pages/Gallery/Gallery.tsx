7a13da1b169f69506e1f630501752e53e8745bfa
Oluwatobiju Judah <tobijudah@gmail.com>
2021-12-17 17:42:15 +0100

    feat: add Gallery component [empty]

---
+++ src/pages/Gallery/Gallery.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../../components/Nav/Nav";
+import { PageProps } from "../page.types";
+
+const Gallery: React.FC<PageProps> = ({ navOnClick, preloaded }) => {
+	return (
+		<div>
+			buhgbhkuvhj
+			<Nav onClick={navOnClick} />
+		</div>
+	);
+};
+
+export default Gallery;
