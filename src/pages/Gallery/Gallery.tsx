ecb22182b669bc831a345803f4697463f74076cf
Oluwatobiju Judah <tobijudah@gmail.com>
2022-01-15 13:02:48 +0100

    feat: add Gallery v1

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,13 +1,125 @@
-import React from "react";
-import Nav from "../../components/Nav/Nav";
+import gsap from "gsap";
+import S from "./Gallery.module.scss";
+import imagesLoaded from "imagesloaded";
 import { PageProps } from "../page.types";
+import Nav from "../../components/Nav/Nav";
+import LocomotiveScroll from "locomotive-scroll";
+import IntroAnimation from "../../animations/intro";
+import React, { useEffect, useRef, useState } from "react";
 
-const Gallery: React.FC<PageProps> = ({ navOnClick, preloaded }) => {
+const Gallery: React.FC<PageProps> = ({
+	appLoaded,
+	navOnClick,
+	preloaded,
+	setAppLoaded,
+}) => {
+	const navRef = useRef(null);
+	const scrollRef = useRef(null);
+	const [scroll, setScroll] = useState<any>();
+
+	const preloadImages = () => {
+		return new Promise((resolve) => {
+			imagesLoaded(
+				document.querySelectorAll(".gallery-image"),
+				{ background: true },
+				resolve
+			);
+		});
+	};
+
+	useEffect(() => {
+		if (preloaded && !scroll) {
+			setScroll(
+				new LocomotiveScroll({
+					smooth: true,
+					el: scrollRef.current,
+					tablet: {
+						smooth: true,
+					},
+					smartphone: {
+						smooth: true,
+					},
+					reloadOnContextChange: true,
+				})
+			);
+		} else if (preloaded && scroll) {
+			Promise.all([preloadImages()]).then(() => {
+				scroll.update();
+			});
+			scroll.stop();
+			scroll.update();
+			const loadedAnimationDelay = window.innerWidth < 1024 ? 0.8 : 0.5;
+			setTimeout(
+				() => {
+					scroll.start();
+					!appLoaded &&
+						window.innerWidth > 1024 &&
+						IntroAnimation(navRef.current);
+					setAppLoaded(true);
+				},
+				!appLoaded ? 0 : loadedAnimationDelay
+			);
+		}
+		return () => scroll && scroll.destroy();
+	}, [scroll, preloaded]);
+
+	useEffect(() => {
+		!appLoaded &&
+			window.innerWidth > 1024 &&
+			gsap.set(navRef.current, { visibility: "hidden" });
+	}, []);
 	return (
-		<div>
-			buhgbhkuvhj
-			<Nav onClick={navOnClick} />
-		</div>
+		<>
+			<Nav ref={navRef} onClick={navOnClick} />
+			<div className={S.gallery}>
+				<h1 className={S.header}>Gallery</h1>
+				<div ref={scrollRef} data-scroll-container>
+					<div className={S.grid}>
+						<div
+							className={S.leftColumn}
+							data-scroll
+							data-scroll-speed={-20}
+						>
+							{Array.from({ length: 11 }, (_, i) => (
+								<img
+									key={i}
+									src={`https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/${
+										i + 1
+									}.png`}
+									alt=""
+								/>
+							))}
+						</div>
+						<div className={S.middleColumn}>
+							{Array.from({ length: 11 }, (_, i) => (
+								<img
+									key={i}
+									src={`https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/${
+										i + 1
+									}.png`}
+									alt=""
+								/>
+							))}
+						</div>
+						<div
+							className={S.rightColumn}
+							data-scroll
+							data-scroll-speed={-20}
+						>
+							{Array.from({ length: 11 }, (_, i) => (
+								<img
+									key={i}
+									src={`https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/${
+										i + 1
+									}.png`}
+									alt=""
+								/>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
