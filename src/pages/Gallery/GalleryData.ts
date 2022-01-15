e48bee5ff41b2858b85ac93b47d2c992d5953e11
Oluwatobiju Judah <tobijudah@gmail.com>
2022-01-15 21:51:08 +0100

    chore: add GalleryData

---
+++ src/pages/Gallery/GalleryData.ts
@@ -0,0 +1,134 @@
+export const GalleryData = [
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/11.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/10.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/9.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/8.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/7.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/6.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/5.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/4.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/3.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/2.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_1/1.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/1.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/2.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/3.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/4.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/5.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/6.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/7.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/8.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/9.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/10.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_2/11.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/11.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/10.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/9.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/8.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/7.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/6.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/5.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/4.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/3.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/2.png",
+	},
+	{
+		name: "",
+		link: "https://res.cloudinary.com/tobijudah/image/upload/q_auto,f_auto/v1642240246/it/gallery_column_3/1.png",
+	},
+];
