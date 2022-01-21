247bf92d6c84223758315a1207496818347a4cd3
Oluwatobiju Judah <tobijudah@gmail.com>
2022-01-21 20:34:03 +0100

    feat: add LongLine animation component

---
+++ src/components/LongLine.tsx
@@ -0,0 +1,36 @@
+import React, { SVGProps } from "react";
+
+type LongLineProps = {
+	windowWidth: number;
+};
+
+export const LongLine = React.forwardRef<
+	SVGSVGElement,
+	SVGProps<SVGSVGElement> & LongLineProps
+>((props, ref) => {
+	return (
+		<svg
+			ref={ref}
+			fill="none"
+			{...props}
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			{props.windowWidth > 600 ? (
+				<line
+					x1="0.5"
+					x2="0.500032"
+					y1="81.111vh"
+					stroke="white"
+					y2="-2.18557e-08"
+				/>
+			) : (
+				<line
+					y1="0.5"
+					y2="0.5"
+					stroke="white"
+					x2="calc(100vw - 3rem)"
+				/>
+			)}
+		</svg>
+	);
+});
