06bbaa365e3463bd9678c9c6c02d611abe11dabe
Oluwatobiju Judah <tobijudah@gmail.com>
2022-02-18 15:55:57 +0100

    feat: update page transition animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import S from "./App.module.scss";
+import splitting from "splitting";
 import Home from "./pages/Home/Home";
 import "splitting/dist/splitting.css";
 import { Route } from "react-router-dom";
@@ -15,6 +16,7 @@ import "../node_modules/locomotive-scroll/src/locomotive-scroll.scss";
 
 function App() {
 	const windowWidth = useWindowWidth();
+	const banner = gsap.utils.selector("#banner");
 	const [open, setOpen] = useState<boolean>(false);
 	const [preloaded, setPreloaded] = useState<boolean>(false);
 	const [appLoaded, setAppLoaded] = useState<boolean>(false);
@@ -26,24 +28,65 @@ function App() {
 	];
 
 	const onEnter = () => {
-		gsap.to("#banner", {
-			delay: 2.5,
-			yPercent: -100,
-			ease: "power2.in",
-		});
-		gsap.set("#banner", {
-			yPercent: 100,
-		});
+		const enter = gsap.timeline();
+		enter
+			.to("#banner", {
+				delay: 2.5,
+				yPercent: -100,
+				ease: "power2.in",
+			})
+			.to(
+				banner(".split-text .word, .whitespace"),
+				{
+					delay: 2.25,
+					duration: 1,
+					opacity: 0,
+					yPercent: -150,
+					stagger: 0.015,
+					ease: "power2.out",
+				},
+				0
+			)
+			.eventCallback("onComplete", () => {
+				gsap.set("#banner", {
+					yPercent: 100,
+				});
+				gsap.set(banner(".split-text .word, .whitespace"), {
+					opacity: 1,
+					yPercent: 0,
+				});
+				enter.kill();
+			});
 	};
 
 	const onExit = () => {
-		gsap.to("#banner", {
+		const exit = gsap.timeline();
+		gsap.set(banner(".hidden-init"), { visibility: "visible" });
+		exit.to("#banner", {
 			yPercent: 0,
 			delay: windowWidth <= 1024 && open ? 0.55 : 0.15,
 			duration: 0.6,
 			ease: "power2.out",
-		});
+		})
+			.from(
+				banner(".split-text .word, .whitespace"),
+				{
+					duration: 1.25,
+					opacity: 0,
+					yPercent: 150,
+					stagger: 0.035,
+					ease: "power2.out",
+				},
+				0.6
+			)
+			.eventCallback("onComplete", () => {
+				exit.kill();
+			});
 	};
+
+	useEffect(() => {
+		splitting({ by: "words", target: "#banner > p" });
+	}, [preloaded]);
 
 	return (
 		<main className={S.app}>
