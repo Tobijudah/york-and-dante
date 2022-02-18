184fdf2193d45d7bec9c4a605fc3e0a479d6659a
Oluwatobiju Judah <tobijudah@gmail.com>
2022-02-18 15:46:35 +0100

    feat: update Banner component w/ text

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import S from "./Banner.module.scss";
 
 const Banner = () => {
-	return <div id="banner" className={S.banner} />;
+	return (
+		<div id="banner" className={S.banner}>
+			<p
+				data-splitting=""
+				className={`${S.textOne} split-text hidden-init`}
+			>
+				York & Dante
+			</p>
+			<p
+				data-splitting=""
+				className={`${S.textTwo} split-text hidden-init`}
+			>
+				Tobiju  X  Felix
+			</p>
+		</div>
+	);
 };
 
 export default Banner;
